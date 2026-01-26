1388fb104e0ed54916823c414ffcc487dc3ef580
niedzwiedzw <niedzwiedzwo@gmail.com>
2026-01-26 17:59:12 +0100

    initial

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        buildInputs = with pkgs; [
+          pkg-config
+          rustToolchain
+        ];
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            inherit buildInputs;
+          };
+      }
+    );
+}
